1ca4252f656aeb74b16efbcaacd3b53f13a65ead
Matias Coronel <elmati.08@hotmail.com>
2024-01-02 16:57:26 -0300

    create proyect vite and install tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <header className='w-full h-24 bg-slate-500'>
+        <div className='lg:w-[80%] m-auto text-3xl font-bold'>
+            hola
+        </div> 
+    </header>
+  )
+}
+
+export default Navbar;
