3ee9dc015d21c57d0991868aee7e6d17e00d14e9
Matias Coronel <elmati.08@hotmail.com>
2024-01-09 16:37:09 -0300

    update contact

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,6 +10,10 @@ import jira from "../assets/jira.png";
 import sass from "../assets/sass.png";
 import tailwind from "../assets/tailwind.png";
 import html from "../assets/html.png"
+import github_Logo from "../assets/github_Logo.png"
+import githubMark from "../assets/githubMark.png"
+import linkedin_logo from "../assets/linkedin_logo.png"
+
 import {handleScroll} from "./hooks/ScrollPage";
 
 const Portfolio = () => {
@@ -49,26 +53,60 @@ const Portfolio = () => {
             </div>
           </div>
           <div className="w-full flex justify-between "> 
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={html} alt="html"/>
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={css} alt="css" />
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={javascript} alt="javascript"/>
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={react} alt="react" />
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={tailwind} alt="tailwind"/>
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={bootstrap} alt="bootstrap"/>
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={sass} alt="sass"/>
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={git} alt="git" />
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={figma} alt="figma" />            
-            <img className="w-[5%] md:w-[4%] lg:w-5%] " src={jira} alt="jira"/>
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={html} alt="html"/>
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={css} alt="css" />
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={javascript} alt="javascript"/>
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={react} alt="react" />
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={tailwind} alt="tailwind"/>
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={bootstrap} alt="bootstrap"/>
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={sass} alt="sass"/>
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={git} alt="git" />
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={figma} alt="figma" />            
+            <img className="w-[3%] md:w-[4%] lg:w-[5%] " src={jira} alt="jira"/>
           </div>
         </div>
         <div id={1} className={`page  flex-col `}>
          
 
-          <h1 className="text-red-500 text-3xl">Work</h1>
+          <h1 className="text-[#016262] text-3xl">Work</h1>
         </div>
 
-        <div id={2} className={`page `}>
-          <h1 className="text-red-500 text-3xl">Contact</h1>
+        <div id={2} className={`h-[100vh] w-full `}>
+          <h1 className="text-[#016262] font-semibold py-10 text-3xl">Contact me</h1>
+          <div className="flex h-full">
+            <div className="w-1/2 flex flex-col">
+                  <a href="https://github.com/MatiasCoronel1312" target="_blank" className="flex border-4 border-black rounded-2xl px-3 py-1 justify-center w-[18rem] mb-12 hover:bg-[#016262]">
+                    <img className="w-[25%]" src={githubMark} alt="github-logo" />
+                    <img className="w-[50%]" src={github_Logo} alt="github" />
+                    
+                  </a>
+                  <a href="https://www.linkedin.com/in/matias-coronel-77a8b822b/" target="_blank" className="border-4 border-black rounded-2xl w-[18rem] h-[5rem] flex items-center hover:bg-[#016262]">
+                    <img className="w-[100%] p-1" src={linkedin_logo} alt="linkedin" />
+                  </a>
+          </div>
+          
+          <div>
+           <form action="https://formsubmit.co/elmati.08@hotmail.com" method="POST">
+              <h4>Email:</h4>
+              <div>
+                  <input type="text" name="nombre" placeholder="Nombre" required/>
+              </div>
+              <div>
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" name="email" id="email" placeholder="Ingresa tu correo" required/>
+              </div>
+              <div>
+                  <textarea name="consulta" cols="30" rows="8"></textarea>
+
+              </div>
+              <div>
+                  <input className="border-2 border-black rounded-3xl px-3 font-semibold hover:bg-[#016262] cursor-pointer mx-1" type="submit" value="Enviar"/>
+                  <input className="border-2 border-black rounded-3xl px-3 font-semibold hover:bg-[#016262] cursor-pointer mx-1" type="reset" value="Reset"/>
+              </div>  
+           </form>
+          </div>
+          </div>
+          
         </div>
       </div>
     </div>
--- src/components/hooks/ScrollPage.jsx
+++ src/components/hooks/ScrollPage.jsx
@@ -4,7 +4,6 @@ export const handleScroll = (event) => {
   
   let deltaYRef = false;
   const currentScrollY = event.deltaY;
-
   if (currentScrollY > 0 && !deltaYRef) {
     ScrollPage("down");
     deltaYRef = true;
@@ -24,36 +23,26 @@ export const handleScroll = (event) => {
 let currentPage=0;
 
  const ScrollPage=(value) => { 
-        
-      console.log('value0',currentPage);
-    
-    
         if (value == "down" && currentPage === 0) {
           currentPage=1          
-          console.log('current1',currentPage);
-          return movePage(1);
-          
+          return movePage(1);          
         }
         if (value == "down" && currentPage === 1) {
           currentPage=2
-          console.log('current2',currentPage);
           return movePage(2);
         }
         if (value == "up" && currentPage === 2) {
           currentPage=1
-          console.log('current1',currentPage);
           return movePage(1);
         }
         if (value == "up" && currentPage === 1) {
           currentPage=0
-          console.log('error0',currentPage);
           return movePage(0);
         }
       
 }
 
 export const movePage = (page) => {
-  console.log('page',page);
   const element = document.getElementById(page);
   if (element) {
     const offset = element.offsetTop;
