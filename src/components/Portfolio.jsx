07bb6bbaac7c66a921f0ecca0b1289566439d180
Matias Coronel <elmati.08@hotmail.com>
2024-01-11 16:52:09 -0300

    update contact me

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,6 +10,8 @@ import jira from "../assets/jira.png";
 import sass from "../assets/sass.png";
 import tailwind from "../assets/tailwind.png";
 import html from "../assets/html.png"
+import copy from "../assets/copy.png"
+
 import github_Logo from "../assets/github_Logo.png"
 import githubMark from "../assets/githubMark.png"
 import linkedin_logo from "../assets/linkedin_logo.png"
@@ -27,6 +29,18 @@ const Portfolio = () => {
       window.removeEventListener("wheel", handleScroll);
     };
   }, []);
+
+
+  const handleCopyClick = () => {
+    const emailToCopy = '5446matias@gmail.com';
+    navigator.clipboard.writeText(emailToCopy)
+      .then(() => {
+        //alert(`¡Correo copiado al portapapeles: ${emailToCopy}`);
+      })
+      .catch((error) => {
+        console.error('Error al copiar al portapapeles:', error);
+      });
+  };
 
   
 
@@ -72,30 +86,40 @@ const Portfolio = () => {
         </div>
 
         <div id={2} className={`h-[100vh] w-full `}>
-          <h1 className="text-[#016262] font-semibold py-10 text-3xl">Contact me</h1>
+          <div className="flex justify-between w-[75%] ">
+            <h1 className="text-[#016262] font-semibold py-10 text-3xl w-1/2">Contact me</h1>
+            <div className=" w-1/2 flex flex-col  justify-end pl-28">
+              <h2 className=" h-8 font-semibold text-xl text-[#016262] ">Write me:</h2>
+              <div className="flex">
+                <p className="font-semibold">5446matias@gmail.com</p>
+                <button onClick={handleCopyClick} className="w-8"><img src={copy} alt="copy" /></button>
+              </div>
+              
+            </div>
+          </div>
+          
           <div className="flex h-full">
             <div className="w-1/2 flex flex-col">
-                  <a href="https://github.com/MatiasCoronel1312" target="_blank" className="flex border-4 border-black rounded-2xl px-3 py-1 justify-center w-[18rem] mb-12 hover:bg-[#016262]">
+                  <a href="https://github.com/MatiasCoronel1312" target="_blank" className="flex border-4 border-black rounded-2xl px-3 py-1 justify-center w-[17rem] mb-12 hover:bg-[#016262]">
                     <img className="w-[25%]" src={githubMark} alt="github-logo" />
                     <img className="w-[50%]" src={github_Logo} alt="github" />
                     
                   </a>
-                  <a href="https://www.linkedin.com/in/matias-coronel-77a8b822b/" target="_blank" className="border-4 border-black rounded-2xl w-[18rem] h-[5rem] flex items-center hover:bg-[#016262]">
+                  <a href="https://www.linkedin.com/in/matias-coronel-77a8b822b/" target="_blank" className="border-4 border-black rounded-2xl w-[17rem] h-[5rem] flex items-center hover:bg-[#016262]">
                     <img className="w-[100%] p-1" src={linkedin_logo} alt="linkedin" />
                   </a>
           </div>
-          <h4>Email:</h4>
           <div>
            <form className="rounded-lg flex flex-col justify-center " action="https://formsubmit.co/elmati.08@hotmail.com" method="POST">
               
               <div className="rounded-lg border-4 border-black mb-1">
-                  <input className="rounded-lg p-1 w-full" type="text" name="nombre" placeholder="Nombre" required/>
+                  <input className="rounded-lg p-1 w-full bg-[#BBDEF0]" type="text" name="nombre" placeholder="Nombre" required/>
               </div>
-              <div className="rounded-lg border-4 border-black mb-1">
-                  <input className="rounded-lg p-1 w-full" type="email" name="email" id="email" placeholder="Email" required/>
+              <div className="rounded-lg border-4 border-black mb-1 ">
+                  <input className="rounded-lg p-1 w-full bg-[#BBDEF0]" type="email" name="email" id="email" placeholder="Email" required/>
               </div>
               <div>
-                  <textarea className="rounded-lg border-4 border-black p-1 h-28 bg-[#BBDEF0]" name="consulta" cols="30" rows="8"></textarea>
+                  <textarea className="rounded-lg border-4 border-black p-1 h-[7rem] bg-[#BBDEF0]" name="consulta" placeholder="Ingrese texto" cols="30" rows="8"></textarea>
               </div>
               <div>
                   <input className="border-4 border-black rounded-xl px-3 font-semibold hover:bg-[#016262] cursor-pointer mx-1" type="submit" value="Enviar"/>
